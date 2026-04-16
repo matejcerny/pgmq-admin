1e8af9758f5741ba8e5d9c1ea20de8c333cc5158
Matej Cerny <cerny.matej@gmail.com>
2026-04-16 14:59:17 +0200

    scala-native 0.5

---
+++ README.md
@@ -0,0 +1,13 @@
+# pgmq-admin 🚧
+
+![](https://img.shields.io/badge/Scala%203-%23de3423.svg?logo=scala&logoColor=white)
+[![Scala Native](https://img.shields.io/badge/scala_native-0.5.11-337ab7?logoColor=white)](https://scala-native.org/)
+
+An admin tool for PGMQ (Postgres Message Queue).
+
+![](docs/_assets/images/pgmq-admin.png)
+
+```shell
+docker compose up
+sbt rootNative/run # or rootJVM/run
+```
--- shared/src/main/scala/io/github/matejcerny/pgmqadmin/App.scala
+++ shared/src/main/scala/io/github/matejcerny/pgmqadmin/App.scala
@@ -3,30 +3,30 @@ package io.github.matejcerny.pgmqadmin
 import cats.effect.{ IO, IOApp }
 import cats.syntax.semigroupk.*
 import io.github.matejcerny.pgmqadmin.config.AppConfig
-import io.github.matejcerny.pgmqadmin.routes.{ DashboardRoutes, MetricRoutes, QueueRoutes, StaticRoutes, TopicRoutes }
+import io.github.matejcerny.pgmqadmin.routes.*
 import io.github.matejcerny.pgmqadmin.services.*
-import natchez.Trace
 import org.http4s.ember.server.EmberServerBuilder
 import org.http4s.server.Router
+import org.typelevel.otel4s.metrics.Meter
+import org.typelevel.otel4s.trace.Tracer
 import pgmq4s.skunk.{ SkunkPgmqAdmin, SkunkPgmqInspector }
 import skunk.Session
 
 object App extends IOApp.Simple:
 
-  import Trace.Implicits.noop
+  import Meter.Implicits.noop
+  import Tracer.Implicits.noop
 
   def run: IO[Unit] =
     val config = AppConfig.default
 
     Session
-      .pooled[IO](
-        host = config.db.host,
-        port = config.db.port,
-        user = config.db.user,
-        database = config.db.database,
-        password = config.db.password,
-        max = config.db.poolSize
-      )
+      .Builder[IO]
+      .withHost(config.db.host)
+      .withPort(config.db.port)
+      .withUserAndPassword(config.db.user, config.db.password)
+      .withDatabase(config.db.database)
+      .pooled(config.db.poolSize)
       .flatMap: pool =>
         val admin = SkunkPgmqAdmin[IO](pool)
         val inspector = SkunkPgmqInspector[IO](pool)
